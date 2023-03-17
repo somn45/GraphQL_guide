9bf8d7e6650c0670d11daca9471c1b56d4e62fe5
somn45 <somnsomn45@gmail.com>
2023-03-17 17:50:23 +0900

    2. GraphQL의 쿼리를 실제로 실행하는 Resolver에 대해 알아보기

--- server.js
+++ server.js
@@ -1,25 +1,66 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+let tweets = [
+  {
+    id: '1',
+    text: 'hello',
+  },
+  {
+    id: '2',
+    text: 'welcome',
+  },
+];
+
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstname: String!
+    lastname: String!
   }
   type Tweet {
-    id: ID
-    text: String
+    id: ID!
+    text: String!
     author: User
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(userId: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(userId: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, args) {
+      console.log(args);
+      return tweets.find((tweet) => tweet.id === args.id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { userId }) {
+      const tweet = tweets.find((tweet) => tweet.id === userId);
+      if (!tweet) return false;
+      tweets.filter((tweet) => tweet.id !== userId);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => console.log(`Running on ${url}`));
